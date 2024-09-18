fbe3c5c39f2e019dd4224a3521a2e77193122e80
Mika Braginsky <mika.br@gmail.com>
2024-09-18 15:46:18 -0400

    first pass germany pilot analyses

---
+++ assessment/germany/scripts/_setup.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(here)
+library(glue)
+library(ggforce)
+library(ggthemes)
+library(mirt)
+library(mgcv)
+
+.font <- "Source Sans Pro"
+theme_set(theme_bw(base_size = 14, base_family = .font))
+theme_update(panel.grid = element_blank(),
+             strip.background = element_blank(),
+             legend.key = element_blank(),
+             panel.border = element_blank(),
+             axis.line = element_line(),
+             strip.text = element_text(face = "bold"))
