97a529cea16c331bfea7ccd2d207fa720aab4089
Mike Frank <mcfrank@stanford.edu>
2024-12-20 22:38:37 -0800

    tom

--- 02_score_data/irt_helpers.R
+++ 02_score_data/irt_helpers.R
@@ -92,7 +92,7 @@ mirt_scores <- function(mod, df, df_prepped) {
   scores <- fscores(mod, method = "MAP", verbose = FALSE)
   user_scores <- tibble(user_id = rownames(df_prepped),
                         ability = scores[,1]) # TODO: check this gives correct order
-  df |> distinct(user_id, run_id, task_id) |>
+  df |> distinct(user_id, run_id) |> # took out task ID here because of multi-task models
     left_join(user_scores) |> select(-task_id)
 }
 
