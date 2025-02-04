8ffa62a1f077e49b8712e598a7306e5a517afb0b
Mika Braginsky <mika.br@gmail.com>
2025-02-04 13:54:25 -0500

    add sample size tabulation scripts

---
+++ 03_summaries/count_scores.R
@@ -0,0 +1,14 @@
+source(here("03_summaries/scores_helper.R"))
+
+scores <- combine_scores()
+
+n_spec <- "n_{str_remove(.col, '_id')}"
+
+scores_counts <- scores |>
+  rename(child_id = user_id) |>
+  group_by(site) |>
+  summarise(across(c("child_id", "run_id", "task"),
+                   n_distinct, .names = n_spec)) |>
+  ungroup()
+
+write_rds(scores_counts, here(glue("02_scored_data/scores_counts.rds")))
