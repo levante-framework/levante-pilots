c0a6d46c9839e706a15388c1b44b41c3785f10d8
Mika Braginsky <mika.br@gmail.com>
2025-04-02 14:53:26 -0400

    rescore us_pilot

--- 03_summaries/plotting_helper.R
+++ 03_summaries/plotting_helper.R
@@ -56,3 +56,22 @@ task_plot_sites <- \(scores, ylab, nr = NULL, nc = NULL, y_axis = seq(6, 14, 2))
          caption = glue("Note: includes only tasks with at least {threshold_n} observations")) +
     guides(colour = "none")
 }
+
+task_plot_completeness <- \(scores, ylab, nr = NULL, nc = NULL, y_axis = seq(6, 14, 2)) {
+  ggplot(scores, aes(x = age, y = metric_value)) +
+    ggh4x::facet_nested_wrap(vars(site_label, task_category, site_task_label),
+                             nrow = nr, ncol = nc,
+                             nest_line = element_line(), solo_line = TRUE,
+                             axes = "x",
+                             scales = "free_y") +
+    geom_point(aes(colour = task_category, shape = completed), alpha = 0.5) +
+    geom_smooth(aes(group = site), method = "gam", colour = "darkgrey", formula = y ~ s(x, bs = "re")) +
+    # geom_smooth(data = scores |> filter(completed),
+    #             aes(group = site), method = "gam", colour = "darkgrey", formula = y ~ s(x, bs = "re")) +
+    # scale_x_continuous(breaks = y_axis) +
+    scale_colour_manual(values = task_pal) +
+    scale_shape_manual(values = c(1, 16), guide = "none") +
+    labs(x = "Age (years)", y = ylab) +
+         # caption = glue("Note: includes only tasks with at least {threshold_n} observations")) +
+    guides(colour = "none")
+}
