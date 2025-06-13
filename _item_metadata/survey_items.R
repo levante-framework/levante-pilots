3628a0859a7b66658976ef6351c382e0492f6631
Mika Braginsky <mika.br@gmail.com>
2025-06-13 13:06:33 -0400

    add survey_part to survey item metadata

--- _item_metadata/survey_items.R
+++ _item_metadata/survey_items.R
@@ -14,21 +14,24 @@ codesheets <- list("caregiver" = "Caregiver Forms",
 get_coding <- function(survey_sheet, survey_type) {
   read_sheet(codebook, sheet = survey_sheet, na = c("", "NA")) |>
     filter(!is.na(variable_name), !str_detect(item_text, "Instructions")) |>
-    select(variable_name, contains("construct"), question_type, reverse_coded,
-           response_options)
+    select(variable_name, contains("construct"), question_type, contains("survey_part"),
+           reverse_coded, response_options)
 }
 survey_coding <- imap(codesheets, get_coding)
 
 # combine codesheets
 survey_items <- survey_coding |>
   bind_rows(.id = "survey_type") |>
+  filter(!str_detect(variable_name, "Intro")) |>
+  mutate(survey_part = if_else(is.na(survey_part), survey_type, survey_part)) |>
   group_by(survey_type) |>
   mutate(variable_order = 1:n()) |>
   ungroup() |>
   mutate(values = response_options |> str_split(", "),
          values_str = if_else(is.na(values), "", map_chr(values, jsonlite::toJSON))) |>
   select(survey_type, variable = variable_name, variable_order,
-         contains("construct"), question_type, reverse_coded, values = values_str)
+         contains("construct"), question_type, survey_part,
+         reverse_coded, values = values_str)
 # write_csv(survey_items, "survey_items.csv", na = "")
 
 # connect to item_metadata redivis dataset, create next version if needed
@@ -36,9 +39,10 @@ item_metadata <- redivis$organization("levante")$dataset("item_metadata:czjv")
 item_metadata <- item_metadata$create_next_version(if_not_exists = TRUE)
 
 # connect to survey_items table, upload new survey_items df
-survey_table <- item_metadata$table("survey_items")
+survey_table <- item_metadata$table("survey_items:tfw0")
 survey_table$update(upload_merge_strategy = "replace")
 survey_table$upload("survey_items")$create(survey_items)
+# st <- survey_table$to_tibble()
 
 # release new item_metadata dataset
 item_metadata$release()
