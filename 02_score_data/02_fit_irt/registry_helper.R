8e5dbae2fd9e5a72bc8a3b29a9c354aefa378146
Mika Braginsky <mika.br@gmail.com>
2025-08-12 10:32:58 -0700

    add model inspection stub

---
+++ 02_score_data/02_fit_irt/registry_helper.R
@@ -0,0 +1,39 @@
+# functions for interfacing with model registry
+
+regdir <- here("02_scoring_outputs", "model_registry")
+
+# list models in registry and parse directory structure into metadata
+list_models <- \() {
+  mod_files <- list.files(regdir, recursive = TRUE)
+  mod_files |>
+    str_split("/", simplify = TRUE) |>
+    as_tibble(.name_repair = "minimal") |>
+    set_names(c("task", "model_set", "subset", "filename")) |>
+    bind_cols(path = mod_files)
+}
+
+# read in files from path column of model df
+load_models <- \(mod_df) {
+  mod_df |> mutate(mod_rec = map(path, \(p) read_rds(file.path(regdir, p))))
+}
+
+# given ModelRecord object, instantiate mirt model object
+model_from_record <- \(mod_rec) {
+  if (model_class(mod_rec) == "SingleGroupClass") {
+    mirt(data = mod_rec@data, pars = model_vals(mod_rec), TOL = NaN)
+  } else if (model_class(mod_rec) == "MultipleGroupClass") {
+    multipleGroup(data = mod_rec@data, group = mod_rec@groups,
+                  pars = model_vals(mod_rec), TOL = NaN)
+  }
+}
+
+# map invariance as recorded in mirt
+# (subset of c("free_means", "free_var", "intercepts", "slopes"))
+# to shorthand name
+# (one of c("configural", "metric", "scalar_intercepts", "scalar_slopes_and_intercepts", "full")
+translate_invariance <- \(invariance_terms) {
+  invariances |>
+    map(\(inv) setequal(inv, invariance_terms)) |>
+    keep(identity) |>
+    names()
+}
