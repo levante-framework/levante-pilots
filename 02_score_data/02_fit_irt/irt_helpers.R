9b52ff623177f75967f3ec1331bd5a31a872a25c
Mika Braginsky <mika.br@gmail.com>
2025-10-06 18:32:17 -0400

    fix model fitting bugs around equality constraints

--- 02_score_data/02_fit_irt/irt_helpers.R
+++ 02_score_data/02_fit_irt/irt_helpers.R
@@ -125,7 +125,8 @@ generate_model_str_numeric <- function(df, df_prepped, item_type, f) { # f = num
   }
   constraints <- items |> map(\(item_uid) {
     # get columns with item's instances
-    matched_idx <- which(str_detect(colnames(df_prepped), paste0(item_uid, item_sep)))
+    # matched_idx <- which(str_detect(colnames(df_prepped), paste0(item_uid, item_sep)))
+    matched_idx <- which(str_detect(colnames(df_prepped), glue("^{item_uid}{item_sep}")))
     
     if (length(matched_idx) > 1) {
       # constraint for item instance: (item_1, item_2, param)
