9e2d084e5ab48d1425d83f8f7e208532cfbbed17
Mika Braginsky <mika.br@gmail.com>
2025-11-14 18:20:18 -0500

    update fetch + scoring_prep for package changes from workflow

--- 02_score_data/01_scoring_prep/scoring_prep_helpers.R
+++ 02_score_data/01_scoring_prep/scoring_prep_helpers.R
@@ -1,59 +1,3 @@
-# recode correctness + reclassify items for HF
-recode_hf <- function(df) {
-  hf_trials <- df |>
-    filter(item_task == "hf") |>
-    # code too fast/slow RTs as incorrect
-    mutate(response_fast = rt_numeric < 200, response_slow = rt_numeric > 2000,
-           correct = correct & !response_fast & !response_slow) |>
-    select(-response_fast, -response_slow) |>
-    # recode items based on whether they're same as previous item
-    group_by(run_id, item_group) |>
-    arrange(trial_number) |>
-    mutate(hf_type = case_when(
-      is.na(lag(item)) ~ "start",
-      item == lag(item) ~ "stay",
-      item != lag(item) ~ "switch")) |>
-    ungroup() |>
-    mutate(item = paste(item, hf_type, sep = "_"),
-           item_uid = paste(item_group, item, sep = "_")) |>
-    # filter(hf_type != "start") |>
-    select(-hf_type)
-
-  df |>
-    filter(item_task != "hf") |>
-    bind_rows(hf_trials)
-}
-
-# recode correctness for slider
-recode_slider <- function(df, threshold) {
-  slider_trials <- df |>
-    filter(item_group == "slider") |>
-    # get target and max values out of item
-    tidyr::separate_wider_delim(item, "_",
-                                names = c("target", "max_value"),
-                                cols_remove = FALSE) |>
-    # convert target and max values to numeric and compute if within threshold
-    mutate(target = target |> stringr::str_replace("^0", "0."),
-           across(c(target, max_value), as.numeric),
-           correct = (abs(as.numeric(response) - target) / max_value < threshold)) |>
-    # remove trials where response greater than max value (must be from a bug)
-    filter(as.numeric(response) <= max_value) |>
-    select(-c("target", "max_value"))
-  df |>
-    filter(item_group != "slider") |>
-    bind_rows(slider_trials)
-}
-
-# recode correctness for items with wrong answers
-recode_wrong_items <- function(df, wrong_items) {
-  wrong_trials <- df |>
-    right_join(wrong_items) |>
-    mutate(correct = !is.na(response) & response == answer_fixed)
-  df |>
-    anti_join(wrong_items) |>
-    bind_rows(wrong_trials)
-}
-
 source("rescore_sds.R")
 recode_sds <- function(df) {
   sds <- df |>
@@ -132,14 +76,4 @@ recode_sds <- function(df) {
   df |>
     filter(item_task != "sds") |>
     bind_rows(sds_trials)
-}
-
-recode_tom <- \(df) {
-  tom <- df |> filter(item_task == "tom")
-  tom_disagg <- tom |>
-    mutate(story = str_extract(item_original, "^[0-9]+"),
-           item_uid = glue("{item_task}_story{story}_{item_group}_{item}"))
-  df |>
-    filter(item_task != "tom") |>
-    bind_rows(tom_disagg)
 }
